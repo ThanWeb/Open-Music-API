06f6fb39054be5c81560a4709c0469310272b4b8
Hans <classsickpeople@gmail.com>
2023-11-01 04:57:15 +0800

    Create mapDBToSongModel in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,4 +14,26 @@ const mapDBToAlbumModel = ({
   updatedAt: updated_at
 })
 
-module.exports = { mapDBToAlbumModel }
+const mapDBToSongModel = ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId,
+  created_at,
+  updated_at
+}) => ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId,
+  createdAt: created_at,
+  updatedAt: updated_at
+})
+
+module.exports = { mapDBToAlbumModel, mapDBToSongModel }
