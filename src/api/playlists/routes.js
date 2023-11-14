c7751b82136b0fa8db27624c58501cce45cbbee9
Hans <classsickpeople@gmail.com>
2023-11-14 11:54:56 +0800

    Create handler and route for playlists plugin

---
+++ src/api/playlists/routes.js
@@ -0,0 +1,19 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/playlists',
+    handler: (request, h) => handler.postPlaylistHandler(request, h)
+  },
+  {
+    method: 'GET',
+    path: '/playlists',
+    handler: (request, h) => handler.getPlaylistsHandler(request, h)
+  },
+  {
+    method: 'DELETE',
+    path: '/playlists/{id}',
+    handler: (request, h) => handler.deletePlaylistByIdHandler(request, h)
+  }
+]
+
+module.exports = routes
