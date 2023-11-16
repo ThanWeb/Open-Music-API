115d38e2ac0f843a25af9222196511a9019e22ce
Hans <classsickpeople@gmail.com>
2023-11-16 23:55:37 +0800

    Create table collaborations migration and update remigrate script

---
+++ migrations/1700102187915_create-table-collaborations.js
@@ -0,0 +1,22 @@
+exports.shorthands = undefined
+
+exports.up = (pgm) => {
+  pgm.createTable('collaborations', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true
+    }
+  })
+}
+
+exports.down = (pgm) => {
+  pgm.dropTable('collaborations')
+}
