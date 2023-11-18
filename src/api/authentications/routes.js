17cbf4900620e53db820cec19ee2c1f3df2adea9
Hans <classsickpeople@gmail.com>
2023-11-18 22:38:06 +0800

    Create documentation for authentications

--- src/api/authentications/routes.js
+++ src/api/authentications/routes.js
@@ -1,18 +1,79 @@
+const Joi = require('joi')
+
 const routes = (handler) => [
   {
     method: 'POST',
     path: '/authentications',
-    handler: (request, h) => handler.postAuthenticationHandler(request, h)
+    handler: (request, h) => handler.postAuthenticationHandler(request, h),
+    options: {
+      tags: ['api'],
+      description: 'Login',
+      validate: {
+        payload: Joi.object({
+          username: 'username',
+          password: 'secret'
+        })
+      },
+      response: {
+        status: {
+          201: Joi.object({
+            status: 'success',
+            message: 'Authentication berhasil ditambahkan',
+            data: {
+              accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzZXItWVRJVWU3NUJrT18xV0IweSIsImlhdCI6MTcwMDIwODA4Mn0.K6W4ZvKqKbNfwqAJA75hQtSJcuxLhzTrnREMLLF9t0E',
+              refreshToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzZXItUU51ZzRtYmZ3UU50cjFndiIsImlhdCI6MTcwMDIwODA4NX0.Mv65VG7VWIpxG-pTvqLvKxHjDfzhKqYWfK7UbAPu15w'
+            }
+          })
+        }
+      }
+    }
   },
   {
     method: 'PUT',
     path: '/authentications',
-    handler: (request, h) => handler.putAuthenticationHandler(request, h)
+    handler: (request, h) => handler.putAuthenticationHandler(request, h),
+    options: {
+      tags: ['api'],
+      description: 'Refresh Authentication',
+      validate: {
+        payload: Joi.object({
+          refreshToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzZXItUU51ZzRtYmZ3UU50cjFndiIsImlhdCI6MTcwMDIwODA4NX0.Mv65VG7VWIpxG-pTvqLvKxHjDfzhKqYWfK7UbAPu15w'
+        })
+      },
+      response: {
+        status: {
+          200: Joi.object({
+            status: 'success',
+            message: 'Access Token berhasil diperbarui',
+            data: {
+              accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzZXItWVRJVWU3NUJrT18xV0IweSIsImlhdCI6MTcwMDIwODA4Mn0.K6W4ZvKqKbNfwqAJA75hQtSJcuxLhzTrnREMLLF9t0E'
+            }
+          })
+        }
+      }
+    }
   },
   {
     method: 'DELETE',
     path: '/authentications',
-    handler: (request, h) => handler.deleteAuthenticationHandler(request, h)
+    handler: (request, h) => handler.deleteAuthenticationHandler(request, h),
+    options: {
+      tags: ['api'],
+      description: 'Delete Authentication',
+      validate: {
+        payload: Joi.object({
+          refreshToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InVzZXItUU51ZzRtYmZ3UU50cjFndiIsImlhdCI6MTcwMDIwODA4NX0.Mv65VG7VWIpxG-pTvqLvKxHjDfzhKqYWfK7UbAPu15w'
+        })
+      },
+      response: {
+        status: {
+          200: Joi.object({
+            status: 'success',
+            message: 'Refresh token berhasil dihapus'
+          })
+        }
+      }
+    }
   }
 ]
 
